03c1d89d8d7d472b8a5ed90eb2a9b7212f8bdac8
Unknown <fossatigiona@gmail.com>
2019-10-02 15:46:57 +0200

    FXML Loader and Controller class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,43 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
+	private Stage primaryStage;
+	private AnchorPane mainLayout;
+	private Scene scene;
+	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		Controller c = new Controller();
+		primaryStage.setTitle("PengWind");
+		showUI();
+	}
+	
+//	FXML SceneBuilder Loader
+	private void showUI() throws IOException {
+		
+//		load the FXML file
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("gameScene.fxml"));
+		mainLayout = loader.load();
+		
+//		add the FXLM SceneBuilder to the Scene
+		scene = new Scene(mainLayout);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		
+//		lock window resize
+		primaryStage.setResizable(false);
+		
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
