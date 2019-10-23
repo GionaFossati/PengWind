fedab2575434af544b37136e09512ee13f27afff
Giona Fossati <fossatigiona@gmail.com>
2019-10-23 11:14:34 +0200

    img path restored

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,7 +2,6 @@ package application;
 	
 import java.io.IOException;
 
-import application.controllers.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
