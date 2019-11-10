f8491c1355c98c3ad093ac7365e7ad30827fd2fa
Giona Fossati <fossatigiona@gmail.com>
2019-11-10 15:59:55 +0100

    C=3

--- src/application/controllers/ControllerMaze.java
+++ src/application/controllers/ControllerMaze.java
@@ -152,7 +152,7 @@ public class ControllerMaze {
 		System.out.println("can move in Y: " + Y);
 		System.out.println("can move in X: " + X);
 		
-		if (X >= 0 && Y >= 0 && X <= 8 && Y <= 8 /*&& getNodeByRowColumnIndex(Y, X) == null*/) {
+		if (X >= 0 && Y >= 0 && X < 9 && Y < 9 /*&& getNodeByRowColumnIndex(Y, X) == null*/) {
 			response = true;
 		} else {
 			response = false;
